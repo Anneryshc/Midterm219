cb325878710ae819eae60945fa33d1e96b1ba2b9
Anneryshc <afh9@njit.edu>
2024-03-27 13:08:34 -0400

    Add content in the menu folder and commands

---
+++ App/CommandLogging/__init__.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+class CommandHandler:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.commands = {}
+        return cls._instance
+
+    def register_command(self, command_name: str, command: Command):
+        self.commands[command_name] = command
+
+    def execute_command(self, command_name: str):
+        try:
+            self.commands[command_name].execute()
+        except KeyError:
+            print(f"No such command: {command_name}")
+
+    def get_available_commands(self):
+        return self.commands
---
+++ App/OptionsPlugins/Menu/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import logging  # Agregar logging
+from App.CommandLogging import CommandHandler
+from App.CommandLogging import Command
+
+class MenuCommand(Command):
+    def execute(self):
+        show_menu()
+
+def show_menu():
+    command_handler = CommandHandler()
+
+    # Obtener la lista de carpetas en la carpeta 'plugins'
+    plugin_folders = [folder for folder in os.listdir('app/plugins') if os.path.isdir(os.path.join('app/plugins', folder))]
+
+    # Registrar el encabezado "Available commands:"
+    logging.info("Available commands:")
+
+    # Escribir los comandos del menú en sys.stdout
+    for plugin_folder in plugin_folders:
+        command = f" - {plugin_folder}"
+        sys.stdout.write(command + '\n')
+        logging.info(command)
