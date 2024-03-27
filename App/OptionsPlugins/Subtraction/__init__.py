4e03fab2078f2f504919fb5b69595ff7443002d5
Anneryshc <afh9@njit.edu>
2024-03-27 13:11:17 -0400

    Add content in the subtraction folder

---
+++ App/OptionsPlugins/Subtraction/__init__.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+from App.CommandLogging import Command
+
+
+class SubtractionCommand(Command):
+    def execute(self):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            result = num1 - num2
+            print(f"the subtraction {num1} and {num2} is : {result}")
+        except ValueError:
+            logging.error("Error: Please enter valid numbers.")
+
+# Llama a la función para ejecutarla directamente al importar el módulo
+if __name__ == "__main__":
+    SubtractionCommand().execute()
