ac756db4936e703da874e7964e46847e7bc741f4
Anneryshc <afh9@njit.edu>
2024-03-28 23:22:49 -0400

    fixed the problem

---
+++ app/OptionsPlugins/Subtraction/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import logging
+from app.CommandLogging import Command
+
+class SubtractionCommand(Command):
+    def execute(self):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            result = num1 - num2
+            print(f"the subtraction {num1} and {num2} is : {result}")
+        except ValueError:
+            logging.error("Error: Please enter valid numbers.")
